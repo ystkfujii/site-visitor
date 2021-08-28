e264ce6056eb6b7b8b09dd94128c758d5d421107
ystkfujii <ystk.fujii0731@gmail.com>
2021-08-28 23:06:44 +0900

    20210828

--- pkg/visitor/visitor.go
+++ pkg/visitor/visitor.go
@@ -1,7 +1,7 @@
 package visitor
 
 import (
-	"fmt"
+	//"fmt"
 	"context"
 	"os"
 	"log"
@@ -30,26 +30,21 @@ func (v *SiteVisitor)Run(ctx context.Context) error {
 	url := "https://books.rakuten.co.jp/rb/16462859/?bkts=1"
 	str := "new_addToCart" 
 
-	ok, _ := httpGate.DoesItContainStr(ctx, url, str)//, 1, 1, str)
-
-	if ok {
-		fmt.Print("OK")
-	} else {
-		fmt.Print("NG")
-	}
-
 	bot, err := linebot.New(
 		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
 	)
-	
 	if err != nil {
 		log.Fatal(err)
 	}
-	message := linebot.NewTextMessage("ps5が売ってるよ!\n" + url)
 	
-	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-		log.Fatal(err)
+	ok, _ := httpGate.DoesItContainStr(ctx, url, str)
+
+	if ok {
+		message := linebot.NewTextMessage("【報告】ps5が現在販売中です\n" + url)
+		if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+			log.Fatal(err)
+		}
 	}
 
 
